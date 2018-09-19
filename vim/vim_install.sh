89ae092eb1da06b0a5bce0c5e06b2cbabc684f46
Mathieu Hilaire <hilaire.mathieu@gmail.com>
2018-09-19 12:38:30 +0100

    Upgrade scripts and vimrc to vim 8

--- vim/vim_install.sh
+++ vim/vim_install.sh
@@ -1,43 +1,22 @@
 #!/bin/bash
 
-# Save script directory
-SCRIPTDIR=$PWD
-
-# Uninstall every instance of vim
-sudo apt-get remove --purge --yes vim vim-runtime vim-gnome vim-tiny vim-common vim-gui-common
-
-# Install dependencies
+# Install vim, pip, git
 sudo apt-get update
-sudo apt-get install --yes liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev
+sudo apt-get install --yes vim python3-pip git python3-setuptools libpython3-dev
 
-cd ~/Downloads
+# Install neovim
+pip3 install wheel
+pip3 install neovim
 
-# Delete previous version of vim
-rm -rf vim
+# Install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# Download last version of vim
-git clone --depth=1 https://github.com/vim/vim
-
-# Configure and install vim
-cd vim/src
-
-make distclean
-
-./configure --with-features=huge --enable-rubyinterp --enable-largefile --disable-netbeans --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu --enable-perlinterp --enable-luainterp --with-luajit --enable-fail-if-missing --enable-cscope
-
-make
-
-sudo make install
-
-# Install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install go autocompletion daemon
+go get -u github.com/mdempsky/gocode
 
 # Setup .vimrc
-cd $SCRIPTDIR
-cp .vimrc ~/.vimrc
-
-# Setup neocomplete configuration
-cp -R matgames ~/.vim/
+ln .vimrc ~/.vimrc
 
 # Install plugins
-vim +PluginInstall +GoInstallBinaries +qall
+vim +PlugInstall +GoInstallBinaries +qall
